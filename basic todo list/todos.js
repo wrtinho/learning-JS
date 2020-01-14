18624475bf91c8655237dc0c9027d909a4932ce9
wrtinho <tinho1068@hotmail.com>
2020-01-14 11:36:10 -0300

    local storage added

--- basic todo list/todos.js
+++ basic todo list/todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-var todos = [
-    'Fazer café',
-     'Estudar JavaScript',
-     'Dormir'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     listElement.innerHTML='';
@@ -37,12 +33,18 @@ function addTodo() {
     var todoText = inputElement.value;
     todos.push(todoText);
     inputElement.value = '';
-    renderTodos();    
+    renderTodos();
+    saveToStorage();    
 }
 buttonElement.onclick= addTodo;
 
 function deleteTodo(pos) {
     todos.splice(pos,1);
     renderTodos();
+    saveToStorage();
     
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
