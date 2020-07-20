f19f4740fa4aabb25d2a336a9dacc66b749c60e0
wellingon <tinho1068@hotmail.com>
2020-07-19 23:55:47 -0300

    Request api github com ajax
    data do user da api github OK,  obs: fix bug ( passar retorno do .then)

--- Desafio 3/ex2.js
+++ Desafio 3/ex2.js
@@ -1,3 +1,14 @@
+function Axios(text) {
+    axios.get(`https://api.github.com/users/${text}`) 
+    .then(function(response) {
+        console.log(response);
+    })
+     .catch(function(error){
+       console.warn(error);
+     }) 
+    
+}
+
 var inputElement = document.querySelector('input[name=user]')
 
 var buttonElement = document.querySelector('button.botao')
@@ -7,10 +18,17 @@ buttonElement.onclick=function(){
     
     var listElement = document.createElement('li');
     
+    var user= Axios(text)
+    console.log("DEU CERTO")
+    console.log()
+
     var textElement = document.createTextNode(text);
     listElement.appendChild(textElement);
 
     var containerElement = document.querySelector('ul')
     containerElement.appendChild(listElement)
+
+    // var HeaderElement = document.querySelector('.header');
+    // containerElement.removeChild(HeaderElement);
 }
  
--- JS assíncrono/main.js
+++ JS assíncrono/main.js
@@ -48,4 +48,4 @@ function Axios() {
      }) 
     
 }
-// Axios();
+Axios();
